b35b17565eb35cc4d8e3f7c1c2a63b014b3d722c
id872 <45285868+id872@users.noreply.github.com>
2021-02-14 17:09:35 +0100

    authgenerator - line too long corrected (pylint warning)

--- authgenerator_app/sql_auth_data.py
+++ authgenerator_app/sql_auth_data.py
@@ -8,12 +8,13 @@ def get_bcrypt_hash(user_pass):
 class SqlAuthData:
     """SQL data for users table for devmon application"""
 
-    def __init__(self, api_key, api_hash, username, user_password, ):
+    def __init__(self, api_key, api_hash, username, user_password):
         self.api_key = api_key
         self.api_hash = api_hash
         self.username = username
         self.user_password_hash = get_bcrypt_hash(user_password).decode()
 
     def __str__(self):
-        return "INSERT INTO `users` (`user_name`, `user_password_hash`, `api_key`, `api_hash`) VALUES\n" \
-               "('{}', '{}', '{}', '{}');".format(self.username, self.user_password_hash, self.api_key, self.api_hash)
+        return "INSERT INTO `users` (`user_name`, `user_password_hash`, `api_key`, `api_hash`)" \
+               "VALUES\n ('{}', '{}', '{}', '{}');" \
+            .format(self.username, self.user_password_hash, self.api_key, self.api_hash)
